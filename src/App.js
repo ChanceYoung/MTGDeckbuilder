56ea0ff1d1ed07bda8a05a371c3f2a9406e8c5d0
Eric Young <Eric.Young@snow.edu>
2021-09-15 15:34:24 -0600

    started laying out wireframe for app, created mock data objects and a state within the App component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import './Styles/custom.scss'
+import decks from './Data/Decks.js'
+import { useState } from 'react'
 
 function App() {
+  const [filteredDecks, setFilteredDecks] = useState(decks)
+
   return (
-    <></>
+    <div className='row'>
+      <div className="col-2">
+        <p>Filter box here</p>
+        <p>Decklist will go here, accordian style</p>
+      </div>
+      <div className="col">
+        <p>Details will go here, this will be a large card with decklist information, the edit details button, and remove button </p>
+        <p>Show card button, which when toggled will show us the list of bootstrap cards that are representative of the cards that exist in the currently selected deck</p>
+      </div>
+    </div>
   );
 }
 
---
+++ src/Data/Decks.js
@@ -0,0 +1,50 @@
+const cards = [
+    {
+        id: 0,
+        name: 'Ajani\'s Welcome',
+        cmc:  '1',
+        set:  'M19',
+        type: 'Enchantment'
+    },
+    {
+        id: 1,
+        name: 'Alley Evasion',
+        cmc: '1',
+        set: 'KLD',
+        type: 'Instant'
+    },
+    {
+        id: 2,
+        name: 'Concordia Pegasus',
+        cmc: '2',
+        set: 'M21',
+        type: 'Creature'
+    },
+    {
+        id: 3,
+        name: 'Containment Priest',
+        cmc: '2',
+        set: 'M21',
+        type: 'Creature'
+    },
+    {
+        id: 4,
+        name: 'Ajani\'s Influence',
+        cmc: '4',
+        set: 'M19',
+        type: 'Sorcery'
+    }
+]
+
+const decks = [
+    {
+        deckid: 0,
+        deckname: '',
+        decktype: '',
+        deckdescription: '',
+        decklist: cards,
+    }
+
+]
+
+export default decks
